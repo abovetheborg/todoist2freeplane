f17c938e9eb819083f21de5eaccbe2ff36915baa
Michel JACQUES <michelm.jacques@gmail.com>
2019-06-02 16:21:32 -0700

    Move the POC code to a class

--- todoist2freeplane/todoist_2_freeplane.py
+++ todoist2freeplane/todoist_2_freeplane.py
@@ -4,54 +4,64 @@ import pprint as pp
 
 from freeplane_schema.freeplane_schema import FreeplaneSchema
 
-#Load the configuration
-with open("todoist_2_freeplane_config.yml", 'r') as ymlfile:
-    CFG = yaml.load(ymlfile, Loader=yaml.FullLoader)
 
-# Establishing connection with Todoist
-todoist_api=todoist.TodoistAPI()
-todoist_api.user.login(CFG['todoist_user'],CFG['todoist_passwd'])
+class TodoistDocument(object):
+    def __init__(self, todoist_user, todoist_password):
+        self.todoist_api = todoist.TodoistAPI()
+        self.todoist_api.user.login(todoist_user, todoist_password)
+        self.remote_data = None
 
-# Creating a Freeplane Document in memory
-fp_doc = FreeplaneSchema()
+    def fetch_remote_data(self):
+        self.remote_data = self.todoist_api.sync()
 
-todoist_all_data = todoist_api.sync()
-todoist_project_data = todoist_all_data['projects']
-todoist_item_data = todoist_all_data['items']
-todoist_notes_data = todoist_all_data['notes']
+    def dump_to_freeplane(self, data, file_location, left_side=list()):
+        fp_doc = FreeplaneSchema()
 
-for prj in todoist_project_data:
-    # print(prj['parent_id'])
-    # print(prj['name'])
-    if prj['parent_id'] is None:
-        id_to_find = "root"
-    else:
-        id_to_find = str(prj['parent_id'])
+        projects = data['projects']
+        tasks = data['items']
+        notes = data['notes']
 
-    fp_doc.add_node_by_id(fp_doc.get_node_by_id(id_to_find), str(prj['id']))
-    fp_doc.set_node_text_by_id(str(prj['id']), prj['name'])
-    fp_doc.set_node_style_by_id(str(prj['id']), "bubble")
-    if prj['name'] in CFG['left_side']:
-        fp_doc.set_node_position_by_id(str(prj['id']), 'left')
+        for prj in projects:
+            if prj['parent_id'] is None:
+                id_to_find = "root"
+            else:
+                id_to_find = str(prj['parent_id'])
 
+            fp_doc.add_node_by_id(fp_doc.get_node_by_id(id_to_find), str(prj['id']))
+            fp_doc.set_node_text_by_id(str(prj['id']), prj['name'])
+            fp_doc.set_node_style_by_id(str(prj['id']), "bubble")
+            if prj['name'] in left_side:
+                fp_doc.set_node_position_by_id(str(prj['id']), 'left')
 
-for item in todoist_item_data:
-    if isinstance(item, dict):
-        id_to_find = str(item['project_id'])
+        for item in tasks:
+            if isinstance(item, dict):
+                id_to_find = str(item['project_id'])
 
-        fp_doc.add_node_by_id(fp_doc.get_node_by_id(id_to_find), str(item['id']))
-        fp_doc.set_node_text_by_id(str(item['id']), item['content'])
-        fp_doc.set_node_style_by_id(str(item['id']), "fork")
-    else:
-        print("Trapped something interesting")
+                fp_doc.add_node_by_id(fp_doc.get_node_by_id(id_to_find), str(item['id']))
+                fp_doc.set_node_text_by_id(str(item['id']), item['content'])
+                fp_doc.set_node_style_by_id(str(item['id']), "fork")
+            else:
+                print("Trapped something interesting")
 
+        for note in notes:
+            fp_doc.add_node_note_by_id(str(note['item_id']), note['content'])
 
-for notes in todoist_notes_data:
-    fp_doc.add_node_note_by_id(str(notes['item_id']), notes['content'])
-
+        fp_doc.write_document(file_location)
 
 
-fp_doc.write_document(CFG['freeplane_location'] + "toto.mm")
+if __name__ == '__main__':
+    # Load the configuration
+    with open("todoist_2_freeplane_config.yml", 'r') as ymlfile:
+        CFG = yaml.load(ymlfile, Loader=yaml.FullLoader)
+
+    # Create Todoist document
+    my_todoist = TodoistDocument(todoist_user=CFG['todoist_user'], todoist_password=CFG['todoist_passwd'])
+
+    # Go get the data
+    my_todoist.fetch_remote_data()
+
+    # Dump the data in freeplane format
+    my_todoist.dump_to_freeplane(my_todoist.remote_data, CFG['freeplane_location'] + "toto2.mm", CFG['left_side'])
 
 
 # Notes:
